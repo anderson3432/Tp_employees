d2084928c59bc6f97cc505ab51fe54ce9bb6beaa
anderson3432 <andersonrandrianoelinarivo@gmail.com>
2026-06-10 12:11:34 +0300

    Modification Anderson

--- employes.php
+++ employes.php
@@ -1,37 +1,46 @@
 <?php
 include('fonction.php');
-// $news = get_all_departement();
 $dept_no = $_GET["dept_no"];
 $employes = get_employes_by_departement($dept_no);
-
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Employés</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="font/bootstrap-icons.css">
 </head>
-<body>
-        <a href="listeDepartements.php"> Retour</a>
-    <h2>Employes du département <?php echo $dept_no; ?></h2>
-    <table border="1">
-        <tr>
-            <th>Numero</th>
-            <th>Prenom</th>
-            <th>Nom</th>
-            <th>Titre</th>
-            <th>Date embauche</th>
-        </tr>
-        <?php foreach($employes as $emp) { ?>
-        <tr>
-            <td><?php echo $emp["emp_no"]; ?></td>
-            <td><?php echo $emp["first_name"]; ?></td>
-            <td><?php echo $emp["last_name"]; ?></td>
-            <td><?php echo $emp["title"]; ?></td>
-            <td><?php echo $emp["hire_date"]; ?></td>
-        </tr>
-        <?php } ?>
+<body class="bg-light">
+
+<div class="container mt-5">
+    <a href="index.php" class="btn btn-secondary mb-3">← Retour</a>
+    <h2 class="mb-4">Employés du département <?php echo $dept_no; ?></h2>
+    <table class="table table-bordered table-striped table-hover">
+        <thead class="table-dark">
+            <tr>
+                <th>Numéro</th>
+                <th>Prénom</th>
+                <th>Nom</th>
+                <th>Titre</th>
+                <th>Date d'embauche</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($employes as $emp) { ?>
+            <tr>
+                <td><?php echo $emp["emp_no"]; ?></td>
+                <td><?php echo $emp["first_name"]; ?></td>
+                <td><?php echo $emp["last_name"]; ?></td>
+                <td><?php echo $emp["title"]; ?></td>
+                <td><?php echo $emp["hire_date"]; ?></td>
+            </tr>
+            <?php } ?>
+        </tbody>
     </table>
+</div>
+
 </body>
 </html>
--- fonction.php
+++ fonction.php
@@ -16,54 +16,6 @@
             }
             return $connect;
         }
-
-
-        function get_login($email,$mdp){
-            $sql = "SELECT * FROM users where email = '%s' AND password = '%s'";
-            $sql = sprintf($sql,$email,$mdp); 
-            // echo $sql;
-            $new_req = mysqli_query(dbconnect(),$sql);
-            $result = mysqli_fetch_assoc($new_req);
-            if ($result == null){
-                return -1;
-            }
-                else {
-                    return 1;
-            }
-        }
-        function get_name($email){
-            $sql = "SELECT username FROM users where email = '%s'";
-            $sql = sprintf($sql,$email);
-            // echo $sql;
-            $new_req = mysqli_query(dbconnect(),$sql);
-            $result = mysqli_fetch_assoc($new_req);
-            return $result; 
-        }
-
-
-        function get_id($email){
-            $sql = "SELECT username FROM users where email = '%s'";
-            $sql = sprintf($sql,$email);
-            // echo $sql;
-            $new_req = mysqli_query(dbconnect(),$sql);
-            $result = mysqli_fetch_assoc($new_req);
-            return $result; 
-        }
-
-        function playlistes(){
-            $sql = 'select * FROM playlists';
-            // echo $sql;
-            $new_req = mysqli_query(dbconnect(),$sql);
-            $result = array();        
-            while ($news = mysqli_fetch_assoc($new_req)) {
-                $result[] = $news;
-            }
-            mysqli_free_result($new_req);
-            return $result;
-
-        }
-
-
         // function get_all_departement(){
         //     $sql = 'select * FROM departments';
         //     // echo $sql;
